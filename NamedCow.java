b1140872d62eafdfd62578731ef00a568c942f28
zixuanshao <31222117+zixuanshao@users.noreply.github.com>
2017-10-18 22:32:05 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,13 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+    private String myname;
+    public NamedCow(String name, String type, String sound)
+    {
+    	super(type, sound);
+    	this.myname = name;
+    }
+    public String getName()
+	{
+		return this.myname;
+	}
 }
